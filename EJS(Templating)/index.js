15ca7e6f32387e922bbb5aab7154bdc2bef651e4
NISCHAY HR <nischayhr11@gmail.com>
2024-08-22 22:59:18 +0530

    Added Instagram Template Into Index File (server)

--- EJS(Templating)/index.js
+++ EJS(Templating)/index.js
@@ -1,8 +1,10 @@
 let express = require("express");
+let data=require("./data.json");
 
 let app=express();
 
 app.set("view engine","ejs");
+app.set("view engine",)
 
 let port=8080;
 
@@ -21,3 +23,16 @@ app.get("/roll",(req,res)=>{
     let roll=Math.floor(Math.random()*6)+1; //data base data
     res.render("roll.ejs",{roll}); // sent in the form of object
 });
+
+app.get("/insta",(req,res)=>{
+
+    res.render("Instagram.ejs");
+})
+
+app.get("/insta/:username",(req,res)=>{
+
+    let {username}=req.params;
+    let datar=data[username];
+    console.log(datar);
+    res.render("Instagram.ejs",{datar});
+})
